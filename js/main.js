50d18c4c0c5bf9aa668456d21c82bdb12fa4c528
NeuralNat <ifrance@yandex.ru>
2024-01-21 23:51:26 +0300

    HTML/CSS/JS .nav-mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-document.querySelector('.nav-icon').addEventListener('click', function(){
-   this.classList.toggle('nav-icon--active');
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav-mobile');
+navIcon.addEventListener('click', function () {
+	this.classList.toggle('nav-icon--active');
+    nav.classList.toggle('nav-mobile--active');
 });
