dd4ac38e6df4522f38e93c80e31023503711d5d0
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-02-22 17:29:41 +0300

    JS inputmask plagin added to contact form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-const navIcon = document.querySelector('.nav-icon');
-const nav = document.querySelector('.nav-mobile');
-navIcon.addEventListener('click', function () {
+document.addEventListener("DOMContentLoaded", function () {
+    // mobile nav
+    const navIcon = document.querySelector('.nav-icon');
+    const nav = document.querySelector('.nav-mobile');
+    navIcon.addEventListener('click', function () {
 	this.classList.toggle('nav-icon--active');
     nav.classList.toggle('nav-mobile--active');
+
+    // phone mask
+    const input = document.querySelector('#phone');
+    const mask = new Inputmask('+7 (999) 999-99-99');
+
+    mask.mask(input);
+});
 });
